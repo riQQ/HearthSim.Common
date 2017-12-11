aeb6be2d34e95e6b5781d62075087bc9bcf1dbda
Alexander Zeier <alex@hearthsim.net>
2017-12-11 06:44:00 +0100

    Ensure Resources/Tiles directory exists

--- generate_resources.bat
+++ generate_resources.bat
@@ -1,4 +1,7 @@
 @echo off
 
+if not exist "/Resources/Tiles" (
+	mkdir "Resources/Tiles"
+)
 msbuild /t:ResourceGenerator /p:Configuration=Debug
 .\ResourceGenerator\bin\x86\Debug\ResourceGenerator.exe .\Resources Tiles
