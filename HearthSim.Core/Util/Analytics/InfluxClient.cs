fec437f563e77723ba2d665ead47df95e0e0edaf
Alexander Zeier <alex@hearthsim.net>
2018-01-06 06:28:21 +0100

    Add InfluxClient

---
+++ HearthSim.Core/Util/Analytics/InfluxClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using HearthSim.Core.Util.Logging;
+
+namespace HearthSim.Core.Util.Analytics
+{
+	public class InfluxClient
+	{
+		private readonly string _hostname;
+		private readonly int _port;
+
+		public InfluxClient(string hostname, int port)
+		{
+			_hostname = hostname;
+			_port = port;
+		}
+
+		public async Task Send(InfluxPointBuilder builder)
+		{
+			await Send(builder.Build());
+		}
+
+		public async Task Send(InfluxPoint point)
+		{
+			try
+			{
+				using(var client = new UdpClient())
+				{
+					var line = point.ToLineProtocol();
+					var data = Encoding.UTF8.GetBytes(line);
+					var length = await client.SendAsync(data, data.Length, _hostname, _port);
+					Log.Debug(line + " - " + length);
+				}
+			}
+			catch(Exception ex)
+			{
+				Log.Debug(ex.ToString());
+			}
+		}
+
+	}
+}
---
+++ HearthSim.Core/Util/Analytics/InfluxPointBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HearthSim.Core.Util.Analytics
+{
+	public class InfluxPointBuilder
+	{
+		private readonly Dictionary<string, object> _fields = new Dictionary<string, object>();
+		private readonly Dictionary<string, object> _tags = new Dictionary<string, object>();
+		private readonly string _name;
+		private DateTime? _timestamp;
+
+		public InfluxPointBuilder(string name)
+		{
+			_name = name;
+			_fields.Add("count", 1);
+		}
+
+		public InfluxPointBuilder Tag(string name, object value)
+		{
+			_tags.Add(name, value);
+			return this;
+		}
+
+		public InfluxPointBuilder Field(string name, object value)
+		{
+			_fields.Add(name, value);
+			return this;
+		}
+
+		public InfluxPointBuilder Timestamp(DateTime timestamp)
+		{
+			_timestamp = timestamp;
+			return this;
+		}
+
+		public InfluxPoint Build()
+		{
+			return new InfluxPoint(_name, _tags, _fields, _timestamp ?? DateTime.UtcNow);
+		}
+	}
+}
