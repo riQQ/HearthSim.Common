d684f807b4692fa76ecfbc030eb1b7731d4e63e8
Alexander Zeier <alex@hearthsim.net>
2018-01-06 01:45:56 +0100

    Make MulliganData fields nullable

--- HearthSim.Core/HSReplay/Data/ArchetypeMulliganData.cs
+++ HearthSim.Core/HSReplay/Data/ArchetypeMulliganData.cs
@@ -14,36 +14,36 @@ namespace HearthSim.Core.HSReplay.Data
 		public int DbfId { get; set; }
 
 		[JsonProperty("winrate_when_drawn")]
-		public double DrawnWinrate { get; set; }
+		public double? DrawnWinrate { get; set; }
 
 		[JsonProperty("opening_hand_winrate")]
-		public double MulliganWinrate { get; set; }
+		public double? MulliganWinrate { get; set; }
 
 		[JsonProperty("winrate_when_played")]
-		public double PlayedWinrate { get; set; }
+		public double? PlayedWinrate { get; set; }
 
 		[JsonProperty("keep_percentage")]
-		public double Kept { get; set; }
+		public double? Kept { get; set; }
 
 		[JsonProperty("times_drawn")]
-		public int TimesDrawn { get; set; }
+		public int? TimesDrawn { get; set; }
 
 		[JsonProperty("times_in_presented_in_initial_cards")]
-		public int TimesInInitialCards { get; set; }
+		public int? TimesInInitialCards { get; set; }
 
 		[JsonProperty("times_in_opening_hand")]
-		public int TimesInOpeningHand { get; set; }
+		public int? TimesInOpeningHand { get; set; }
 
 		[JsonProperty("times_kept")]
-		public int TimesKept { get; set; }
+		public int? TimesKept { get; set; }
 
 		[JsonProperty("times_played")]
-		public int TimesPlayed { get; set; }
+		public int? TimesPlayed { get; set; }
 
 		[JsonProperty("avg_turn_played_on")]
-		public double TurnPlayed { get; set; }
+		public double? TurnPlayed { get; set; }
 
 		[JsonProperty("avg_turns_in_hand")]
-		public double TurnsInHand { get; set; }
+		public double? TurnsInHand { get; set; }
 	}
 }
