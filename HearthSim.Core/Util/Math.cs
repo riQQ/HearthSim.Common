d7c2edf88e7c397c4e259c88ad8a657f7aaf0500
Alexander Zeier <alex@hearthsim.net>
2018-01-20 19:40:05 +0100

    Fix DrawProbability for more draw or copies than cards in deck

--- HearthSim.Core/Util/Math.cs
+++ HearthSim.Core/Util/Math.cs
@@ -3,7 +3,11 @@
 	public static class Math
 	{
 		public static double DrawProbability(int copies, int deck, int draw)
-			=> 1 - (BinomialCoefficient(deck - copies, draw) / BinomialCoefficient(deck, draw));
+		{
+			if(draw >= deck || copies >= deck)
+				return 1;
+			return 1 - (BinomialCoefficient(deck - copies, draw) / BinomialCoefficient(deck, draw));
+		}
 
 		public static double BinomialCoefficient(int n, int k)
 		{
