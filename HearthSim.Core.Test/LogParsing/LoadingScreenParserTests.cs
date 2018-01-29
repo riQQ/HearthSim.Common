28a4a237afb69d02ff31f2e661dde9a6b7776651
Alexander Zeier <alex@hearthsim.net>
2018-01-29 13:42:56 +0100

    Add empty tests for remaining log parsers

---
+++ HearthSim.Core.Test/LogParsing/LoadingScreenParserTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HearthSim.Core.Test.LogParsing
+{
+	[TestClass]
+	public class LoadingScreenParserTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
