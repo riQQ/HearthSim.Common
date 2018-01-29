a58618d0e59bb5bc0f2710983b4b43fccecf6ba3
Alexander Zeier <alex@hearthsim.net>
2018-01-30 00:38:05 +0100

    Add DungeonRunWatcher with tests

---
+++ HearthSim.Core.Test/Watchers/DungeonRunWatcherTests.cs
@@ -0,0 +1,185 @@
+﻿
+using System.Collections.Generic;
+using System.Threading;
+using HearthDb;
+using HearthMirror.Objects;
+using HearthSim.Core.Util.Watchers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HearthSim.Core.Test.Watchers
+{
+	[TestClass]
+	public class DungeonRunWatcherTests
+	{
+		private DungeonRunWatcher.RunType? _run;
+		private DungeonInfo _info;
+		private MockDungeonRunData _data;
+		private DungeonRunWatcher _watcher;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_run = null;
+			_info = null;
+			_data = new MockDungeonRunData();
+			_watcher = new DungeonRunWatcher(_data, 100);
+			_watcher.DungeonRunMatchStarted += run => _run = run;
+			_watcher.DungeonInfoChanged += info => _info = info;
+		}
+
+		[TestMethod]
+		public void Update_Noop()
+		{
+			var result = _watcher.Update();
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+			Assert.IsNull(_run);
+			Assert.IsNull(_info);
+		}
+
+		[TestMethod]
+		public void InAdventureScreen_NoData_Noop()
+		{
+			_data.InAdventureScreen = true;
+			var result = _watcher.Update();
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+		}
+
+		[TestMethod]
+		public void InAdventureScreen_RunInactive_Noop()
+		{
+			_data.InAdventureScreen = true;
+			var result = _watcher.Update();
+			_data.MockDungeonInfo = new MockDungeonInfo
+			{
+				RunActive = false
+			};
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+		}
+
+		[TestMethod]
+		public void InAdventureScreen_RunActiveNotChosen_NewData()
+		{
+			_data.InAdventureScreen = true;
+			_data.MockDungeonInfo = new MockDungeonInfo
+			{
+				RunActive = true,
+				DbfIds = new List<int> {1, 2, 3},
+			};
+			var result = _watcher.Update();
+			Assert.IsNotNull(_info);
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+		}
+
+		[TestMethod]
+		public void InAdventureScreen_RunActiveChosen_NewData()
+		{
+			_data.InAdventureScreen = true;
+			_data.MockDungeonInfo = new MockDungeonInfo
+			{
+				RunActive = true,
+				DbfIds = new List<int> {1, 2, 3},
+				PlayerChosenLoot = 1,
+				PlayerChosenTreasure = 1
+			};
+			var result = _watcher.Update();
+			Assert.IsNotNull(_info);
+			Assert.AreEqual(Watcher.UpdateResult.Break, result);
+		}
+
+		[TestMethod]
+		public void InAdventureScreen_InfoNotUpdatedUntilReset()
+		{
+			_data.InAdventureScreen = true;
+			_data.MockDungeonInfo = new MockDungeonInfo
+			{
+				RunActive = true,
+				DbfIds = new List<int> {1, 2, 3},
+				PlayerChosenLoot = 1,
+				PlayerChosenTreasure = 1
+			};
+
+			_watcher.Update();
+			Assert.IsNotNull(_info);
+
+			_info = null;
+			_watcher.Update();
+			Assert.IsNull(_info);
+
+			_watcher.Run();
+			Thread.Sleep(130);
+			Assert.IsNotNull(_info);
+		}
+
+		[TestMethod]
+		public void InMatch_NoData_Noop()
+		{
+			_data.InAiMatch = true;
+			var result = _watcher.Update();
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+		}
+
+		[TestMethod]
+		public void InMatch_NonBossHero_Noop()
+		{
+			_data.InAiMatch = true;
+			_data.OpponentHeroId = "HERO_01";
+			var result = _watcher.Update();
+			Assert.AreEqual(Watcher.UpdateResult.Continue, result);
+		}
+
+		[TestMethod]
+		public void InMatch_InitialBossHero_NewRun()
+		{
+			_data.InAiMatch = true;
+			_data.OpponentHeroId = CardIds.NonCollectible.Hunter.GiantRatHeroic;
+			var result = _watcher.Update();
+			Assert.AreEqual(DungeonRunWatcher.RunType.New, _run);
+			Assert.AreEqual(Watcher.UpdateResult.Break, result);
+		}
+
+		[TestMethod]
+		public void InMatch_LateBossHero_NewRun()
+		{
+			_data.InAiMatch = true;
+			_data.OpponentHeroId = CardIds.NonCollectible.Hunter.TrappedRoomHeroic;
+			var result = _watcher.Update();
+			Assert.AreEqual(DungeonRunWatcher.RunType.Existing, _run);
+			Assert.AreEqual(Watcher.UpdateResult.Break, result);
+		}
+	}
+
+	public class MockDungeonRunData : DungeonRunWatcher.IDungeonRunDataProvider
+	{
+		public bool InAiMatch { get; set; }
+		public bool InAdventureScreen { get; set; }
+		public string OpponentHeroId { get; set; }
+		public MockDungeonInfo MockDungeonInfo { get; set; }
+		public DungeonInfo GetDungeonInfo()
+		{
+			return MockDungeonInfo;
+		}
+	}
+
+	public class MockDungeonInfo : DungeonInfo
+	{
+		public new bool RunActive
+		{
+			set => _runActive = value;
+		}
+
+		public new List<int> DbfIds
+		{
+			set => _dbfIds = value;
+		}
+
+		public new int PlayerChosenLoot
+		{
+			set => _playerChosenLoot = value;
+		}
+
+		public new int PlayerChosenTreasure
+		{
+			set => _playerChosenTreasure = value;
+		}
+	}
+}
---
+++ HearthSim.Core/Util/Watchers/DungeonWatcher.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HearthDb;
+using HearthDb.Enums;
+using HearthMirror.Objects;
+
+namespace HearthSim.Core.Util.Watchers
+{
+	public class DungeonRunWatcher : Watcher
+	{
+		private readonly IDungeonRunDataProvider _dataProvider;
+
+		private readonly string[] _initialOpponents =
+		{
+			CardIds.NonCollectible.Rogue.BinkTheBurglarHeroic,
+			CardIds.NonCollectible.Hunter.GiantRatHeroic,
+			CardIds.NonCollectible.Hunter.WeeWhelpHeroic
+		};
+
+		private List<int> _prevCards;
+		private int _prevLootChoice;
+		private int _prevTreasureChoice;
+
+		public DungeonRunWatcher(IDungeonRunDataProvider dataProvider, int updateDelay = 500) : base(updateDelay)
+		{
+			_dataProvider = dataProvider;
+		}
+
+		public event Action<DungeonInfo> DungeonInfoChanged;
+		public event Action<RunType> DungeonRunMatchStarted;
+
+		protected override void Reset()
+		{
+			_prevCards = null;
+			_prevLootChoice = 0;
+			_prevTreasureChoice = 0;
+		}
+
+		public override UpdateResult Update()
+		{
+			if(_dataProvider.InAdventureScreen)
+			{
+				var dungeonInfo = _dataProvider.GetDungeonInfo();
+				if(dungeonInfo?.RunActive ?? false)
+				{
+					if(_prevCards == null || !dungeonInfo.DbfIds.SequenceEqual(_prevCards)
+										|| _prevLootChoice != dungeonInfo.PlayerChosenLoot
+										|| _prevTreasureChoice != dungeonInfo.PlayerChosenTreasure)
+					{
+						_prevCards = dungeonInfo.DbfIds.ToList();
+						_prevLootChoice = dungeonInfo.PlayerChosenLoot;
+						_prevTreasureChoice = dungeonInfo.PlayerChosenTreasure;
+						DungeonInfoChanged?.Invoke(dungeonInfo);
+						if(_prevLootChoice > 0 && _prevTreasureChoice > 0)
+							return UpdateResult.Break;
+					}
+				}
+				else
+					_prevCards = null;
+			}
+			else if(_dataProvider.InAiMatch && !string.IsNullOrEmpty(_dataProvider.OpponentHeroId))
+			{
+				if(Cards.All.TryGetValue(_dataProvider.OpponentHeroId, out var card))
+				{
+					if(card.Set == CardSet.LOOTAPALOOZA && card.Id.Contains("BOSS"))
+					{
+						var newRun = _initialOpponents.Contains(_dataProvider.OpponentHeroId);
+						DungeonRunMatchStarted?.Invoke(newRun ? RunType.New : RunType.Existing);
+						return UpdateResult.Break;
+					}
+				}
+			}
+
+			return UpdateResult.Continue;
+		}
+
+		public enum RunType
+		{
+			New,
+			Existing
+		}
+
+		public interface IDungeonRunDataProvider
+		{
+			bool InAiMatch { get; }
+			bool InAdventureScreen { get; }
+			string OpponentHeroId { get; }
+			DungeonInfo GetDungeonInfo();
+		}
+	}
+}
