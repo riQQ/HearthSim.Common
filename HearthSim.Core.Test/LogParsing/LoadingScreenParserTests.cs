7f6d9b337bbe1c07e0b1e658261d49fb27a9fc05
Alexander Zeier <alex@hearthsim.net>
2018-02-02 12:30:55 +0100

    Add LoadingScreen tests

--- HearthSim.Core.Test/LogParsing/LoadingScreenParserTests.cs
+++ HearthSim.Core.Test/LogParsing/LoadingScreenParserTests.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using HearthSim.Core.Hearthstone.Enums;
+using HearthSim.Core.LogParsing.Parsers;
+using HearthSim.Core.LogReading.Data;
+using HearthSim.Core.Util.EventArgs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HearthSim.Core.Test.LogParsing
@@ -7,9 +11,25 @@ namespace HearthSim.Core.Test.LogParsing
 	public class LoadingScreenParserTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void ModeChanged()
 		{
-			throw new NotImplementedException();
+			ModeChangedEventArgs modeArgs = null;
+			var parser = new LoadingScreenParser();
+			parser.ModeChanged += args => modeArgs = args;
+			parser.Parse(new Line("LoadingScreen", "D 12:09:10.8365733 LoadingScreen.OnSceneLoaded() - prevMode=GAMEPLAY currMode=TOURNAMENT"));
+			Assert.IsNotNull(modeArgs);
+			Assert.AreEqual(Mode.GAMEPLAY, modeArgs.PreviousMode);
+			Assert.AreEqual(Mode.TOURNAMENT, modeArgs.CurrentMode);
+		}
+
+		[TestMethod]
+		public void GameplayStart()
+		{
+			var invoked = false;
+			var parser = new LoadingScreenParser();
+			parser.GameplayStart += () => invoked = true;
+			parser.Parse(new Line("LoadingScreen", "D 12:04:32.9074527 Gameplay.Start()"));
+			Assert.IsTrue(invoked);
 		}
 	}
 }
