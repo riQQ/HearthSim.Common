4d712a66157e332845dbe2cdea50402deb7f3187
Alexander Zeier <alex@hearthsim.net>
2018-02-14 14:32:28 -0700

    Add more game events

--- HearthSim.Core/Hearthstone/CommonGameEventArgs.cs
+++ HearthSim.Core/Hearthstone/CommonGameEventArgs.cs
@@ -13,4 +13,18 @@ namespace HearthSim.Core.Hearthstone
 			GameState = state;
 		}
 	}
+
+	public class AttackEventArgs : EventArgs
+	{
+		public int Attacker { get; }
+		public int Defender { get; }
+		public IGameState GameState { get; }
+
+		public AttackEventArgs(int attacker, int defender, IGameState state)
+		{
+			Attacker = attacker;
+			Defender = defender;
+			GameState = state;
+		}
+	}
 }
--- HearthSim.Core/Hearthstone/GameEvents.cs
+++ HearthSim.Core/Hearthstone/GameEvents.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using HearthDb.Enums;
 using HearthSim.Core.Hearthstone.GameStateModifiers;
 using HearthSim.Core.Util.EventArgs;
 
@@ -13,6 +15,21 @@ namespace HearthSim.Core.Hearthstone
 		{
 			_actions = new Dictionary<GameTagChange, Action<CommonGameEventArgs>>
 			{
+				[(GameTag.ZONE, Zone.HAND, Zone.PLAY)] = args => CardPlayed?.Invoke(args),
+				[(GameTag.ZONE, Zone.DECK, Zone.HAND)] = args => CardDrawn?.Invoke(args),
+				[(GameTag.ZONE, Zone.HAND, Zone.DECK)] = args => CardMulliganed?.Invoke(args),
+				[(GameTag.ZONE, Zone.HAND, Zone.GRAVEYARD)] = args => CardDiscarded?.Invoke(args),
+				[(GameTag.ZONE, Zone.DECK, Zone.GRAVEYARD)] = args => CardMilled?.Invoke(args),
+				[(GameTag.ZONE, Zone.PLAY, Zone.HAND)] = args => MinionReturnedToDeck?.Invoke(args),
+				[(GameTag.ZONE, Zone.PLAY, Zone.GRAVEYARD)] = args => MinionDied?.Invoke(args),
+				[(GameTag.ZONE, Zone.PLAY, Zone.HAND)] = args => MinionReturnedToHand?.Invoke(args),
+				[(GameTag.ZONE, Zone.INVALID, Zone.DECK)] = args => CardCreatedInDeck?.Invoke(args),
+				[(GameTag.ZONE, Zone.SETASIDE, Zone.DECK)] = args => CardCreatedInDeck?.Invoke(args),
+				[(GameTag.ZONE, Zone.INVALID, Zone.HAND)] = args => CardCreatedInHand?.Invoke(args),
+				[(GameTag.ZONE, Zone.SETASIDE, Zone.HAND)] = args => CardCreatedInHand?.Invoke(args),
+				[(GameTag.ZONE, Zone.INVALID, Zone.PLAY)] = args => CardCreatedInPlay?.Invoke(args),
+				[(GameTag.ZONE, Zone.SETASIDE, Zone.PLAY)] = args => CardCreatedInPlay?.Invoke(args),
+				[(GameTag.ZONE, Zone.DECK, Zone.PLAY)] = args => CardPlayedFromDeck?.Invoke(args),
 			};
 		}
 
@@ -40,6 +57,20 @@ namespace HearthSim.Core.Hearthstone
 		public event Action SetupComplete;
 		public event Action<ActivePlayerDeckChangedEventArgs> ActivePlayerDeckChanged;
 
+		public event Action<CommonGameEventArgs> CardDrawn;
+		public event Action<CommonGameEventArgs> CardPlayed;
+		public event Action<CommonGameEventArgs> CardDiscarded;
+		public event Action<CommonGameEventArgs> CardMilled;
+		public event Action<CommonGameEventArgs> CardMulliganed;
+		public event Action<CommonGameEventArgs> MinionReturnedToDeck;
+		public event Action<CommonGameEventArgs> MinionDied;
+		public event Action<CommonGameEventArgs> MinionReturnedToHand;
+		public event Action<CommonGameEventArgs> CardCreatedInDeck;
+		public event Action<CommonGameEventArgs> CardCreatedInHand;
+		public event Action<CommonGameEventArgs> CardCreatedInPlay;
+		public event Action<CommonGameEventArgs> CardPlayedFromDeck;
+		public event Action<AttackEventArgs> Attack;
+
 		internal virtual void OnSetupComplete() => SetupComplete?.Invoke();
 		internal virtual void OnModeChanged(ModeChangedEventArgs args) => ModeChanged?.Invoke(args);
 		internal virtual void OnGameStateChanged(GameStateChangedEventArgs args) => GameStateChanged?.Invoke(args);
@@ -64,6 +95,12 @@ namespace HearthSim.Core.Hearthstone
 		{
 			if(t.EntityId.HasValue && _actions.TryGetValue((t.Tag, t.PreviousValue ?? 0, t.Value), out var action))
 				action?.Invoke(new CommonGameEventArgs(t.EntityId.Value, state));
+			if(t.Tag == GameTag.DEFENDING)
+			{
+				var attacker = state.Entities.FirstOrDefault(x => x.Value.HasTag(GameTag.ATTACKING));
+				if(t.EntityId.HasValue && attacker.Value != null)
+					Attack?.Invoke(new AttackEventArgs(attacker.Value.Id, t.EntityId.Value, state));
+			}
 		}
 	}
 }
