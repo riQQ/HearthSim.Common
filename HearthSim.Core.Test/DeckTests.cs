68a5ea0db31ca608095f29925937227cfbcf48f4
Alexander Zeier <alex@hearthsim.net>
2018-04-19 14:05:42 +0200

    Fix deckstring test for new standard year

--- HearthSim.Core.Test/DeckTests.cs
+++ HearthSim.Core.Test/DeckTests.cs
@@ -11,7 +11,7 @@ namespace HearthSim.Core.Test
 		[TestMethod]
 		public void GetDeckstring()
 		{
-			const string deckstring = "AAECAYDTAgSyAq8EkbwCnOICDbQB7QKoBdQF3QiStgKBwgLrwgKbywLKywKmzgL70wLb4wIA";
+			const string deckstring = "AAEBAYDTAgSyAq8EkbwCnOICDbQB7QKoBdQF3QiStgKBwgLrwgKbywLKywKmzgL70wLb4wIA";
 			var hdbDeck = DeckSerializer.Deserialize(deckstring);
 			var deck = new Hearthstone.Deck(hdbDeck);
 			Assert.AreEqual(deckstring, deck.GetDeckstring());
