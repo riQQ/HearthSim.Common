4beea55e58fda4fa27defd2ad6d02861b1f03944
Alexander Zeier <alex@hearthsim.net>
2018-04-21 00:48:23 +0200

    Import HearthstoneRunner from HDT

---
+++ HearthSim.Util/HearthstoneRunner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using HearthSim.Util.Logging;
+
+namespace HearthSim.Util
+{
+	public static class HearthstoneRunner
+	{
+		public enum State
+		{
+			Starting,
+			Success,
+			Error
+		}
+
+		private const int ProcCheckInterval = 500;
+		private const int ProcExistDuration = 7000;
+		private const int TryStartBnetDuration = 20000;
+		private const int StartBnetTries = TryStartBnetDuration / ProcCheckInterval;
+		private const int EventDelay = 2000;
+
+		private static bool _starting;
+		private static readonly string[] ProcNames = { "Battle.net", "Battle.net.beta" };
+		public static Action<State> StartingHearthstone;
+
+		public static async Task StartHearthstone()
+		{
+			if(_starting || HearthstoneWindow.Get() != IntPtr.Zero)
+				return;
+			var foundDuration = 0;
+			_starting = true;
+			StartingHearthstone?.Invoke(State.Starting);
+			try
+			{
+				var bnetProc = GetProcess();
+				if(bnetProc == null)
+				{
+					Process.Start("battlenet://");
+					for(var i = 0; i < StartBnetTries; i++)
+					{
+						bnetProc = GetProcess();
+						if(bnetProc != null && bnetProc.MainWindowHandle != IntPtr.Zero)
+							if((foundDuration += ProcCheckInterval) >= ProcExistDuration)
+								break;
+						await Task.Delay(ProcCheckInterval);
+					}
+
+					if(foundDuration == 0)
+						return;
+				}
+
+				Process.Start("battlenet://WTCG");
+			}
+			catch(Exception ex)
+			{
+				Log.Error(ex);
+			}
+			finally
+			{
+				_starting = false;
+				await Task.Delay(EventDelay);
+				StartingHearthstone?.Invoke(foundDuration == 0 ? State.Error : State.Success);
+			}
+		}
+
+		private static Process GetProcess()
+		{
+			return ProcNames.Select(x => Process.GetProcessesByName(x).FirstOrDefault()).FirstOrDefault(x => x != null);
+		}
+	}
+}
