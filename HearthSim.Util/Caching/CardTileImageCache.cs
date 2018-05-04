c16df716aceab7470489119869ad160ac71870c7
Alexander Zeier <alex@hearthsim.net>
2018-05-03 18:41:34 -0600

    Add placeholder for card tile images

--- HearthSim.Util/Caching/CardTileImageCache.cs
+++ HearthSim.Util/Caching/CardTileImageCache.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Windows.Media.Imaging;
 
 namespace HearthSim.Util.Caching
 {
 	public class CardTileImageCache : BitmapImageCache
 	{
 		private static readonly Func<string, string> KeyToUrl = cardId => $"https://art.hearthstonejson.com/v1/tiles/{cardId}.jpg";
+
+		protected override BitmapImage GetPlaceholder(string key)
+		{
+			return new BitmapImage(new Uri("pack://application:,,,/HearthSim.Util;Component/Resources/loading_tile.jpg"));
+		}
 
 		public CardTileImageCache(string persistentDirectory) : base(500, persistentDirectory, KeyToUrl)
 		{
